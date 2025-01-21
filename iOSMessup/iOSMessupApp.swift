67b7808b18de033b4c93323f906f937b736a9658
RafaSG13 <=>
2025-01-21 22:11:37 +0100

    Initial Commit

---
+++ iOSMessup/iOSMessupApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSMessupApp.swift
+//  iOSMessup
+//
+//  Created by Rafael Serrano Gamarra on 21/1/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSMessupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
