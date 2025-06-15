13c54e22594f21e3a91cd51a5decf0c35edab853
RafaSG13 <rafasrrg13@gmail.com>
2025-06-15 17:45:02 +0200

    Create settings bundle to change environment

---
+++ iOSMessup/Core/Environment/MUEnvironmentConfigurator.swift
@@ -0,0 +1,42 @@
+//
+//  AppEnvironment.swift
+//  iOSMessup
+//
+//  Created by Rafael Serrano Gamarra on 15/6/25.
+//
+
+
+import Foundation
+import SwiftUI
+
+enum EnvironmentKey: String {
+    case dev
+    case prod
+}
+
+// El gestor de nuestro entorno
+@MainActor
+final class MUEnvironmentConfigurator {
+    let expenseRepository: ExpenseRepository
+    let savingViewModel: SavingViewModel
+    let authenticationService: AuthenticationService
+
+    init() {
+        let environmentKey = EnvironmentKey(rawValue: UserDefaults.standard.string(forKey: "messup_environment") ?? "dev")
+
+        switch environmentKey {
+        case .dev:
+            self.authenticationService = AuthenticationService(dataSource: MockAuthenticationDataSource(),
+                                                               tokenStorage: InMemoryTokenStorage())
+            self.expenseRepository = ExpenseRepository(dataSource: MockExpensesDataSource())
+            self.savingViewModel = SavingViewModel(dataSource: SavingDataSource())
+        case .prod:
+            self.authenticationService = AuthenticationService(dataSource: LiveAuthenticationDataSource(),
+                                                               tokenStorage: UserDefaultsTokenStorage())
+            self.expenseRepository = ExpenseRepository(dataSource: ExpensesDataSource())
+            self.savingViewModel = SavingViewModel(dataSource: SavingDataSource())
+        case .none:
+            fatalError("Unknown app environment")
+        }
+    }
+}
